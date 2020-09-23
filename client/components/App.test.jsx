37585a086125d9e6eea39eec9247af8101b36b68
chaz-ratima <chaz@ChazPC.localdomain>
2020-09-23 12:52:05 +1200

    add styling

--- client/components/App.test.jsx
+++ client/components/App.test.jsx
@@ -6,7 +6,7 @@ import App from './App'
 jest.mock('./Home.jsx', () => { return () => <h1>Home Page</h1> })
 
 test('renders home', () => {
-    render(<App />)
-    let heading = screen.getAllByRole('heading')
-    expect(heading[0].innerHTML).toMatch(/Home Page/)
+  render(<App />)
+  let heading = screen.getAllByRole('heading')
+  expect(heading[0].innerHTML).toMatch(/Home Page/)
 })
--- client/components/Home.test.jsx
+++ client/components/Home.test.jsx
@@ -5,8 +5,8 @@ import Home from './Home'
 
 jest.mock('./Recipes.jsx', () => { return () => <h1>Recipes</h1> })
 
-test('renders Recipes', () => {
+test('renders heading in Home', () => {
   render(<Home />)
   let heading = screen.getAllByRole('heading')
-  expect(heading[0].innerHTML).toMatch(/Recipes/)
+  expect(heading[1].innerHTML).toMatch(/Recipes/)
 })
