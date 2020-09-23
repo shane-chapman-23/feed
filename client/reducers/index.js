97ba0d80abc84e8213671407800f781f83d5a458
chaz-ratima <chaz@ChazPC.localdomain>
2020-09-24 11:08:00 +1200

    merge:

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,17 +1,7 @@
 import {combineReducers} from 'redux'
-<<<<<<< HEAD
-import recipe from './recipe'
-
-const reducers = combineReducers({
-  recipe
-})
-
-export default reducers
-=======
 
 import recipesReducer from './recipes'
 
 export default combineReducers({
     recipesReducer
 })
->>>>>>> 370150090ea0a36ff1024f57b2079ca5000e8c80
