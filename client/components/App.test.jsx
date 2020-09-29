9af332029e602a4ff1ebd40f9d5c4a20fcb6fe59
Shane Chapman <shane@Shanes-MacBook-Pro.local>
2020-09-29 14:43:06 +1300

    fixed the tests

--- client/components/App.test.jsx
+++ client/components/App.test.jsx
@@ -6,11 +6,11 @@ import store from '../store'
 import App from './App'
 
 
-jest.mock('./Home.jsx', () => { return () => <h1>Home Page</h1> })
+jest.mock('./RecipeList.jsx', () => { return () => <h1 role='heading'>Recipes</h1> })
 
 test('renders home', () => {
   render(<Provider store={store}><App /></Provider>)
   let heading = screen.getAllByRole('heading')
-  expect(heading[0].innerHTML).toMatch(/Home Page/)
+  expect(heading[0].innerHTML).toMatch(/Recipes/)
 })
 
