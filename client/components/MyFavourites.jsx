fbaa3040f6e210a47935f95ae486e9f1da11e039
Shane Chapman <shane@Shanes-MacBook-Pro.local>
2020-09-30 10:13:34 +1300

    fixed the delete function(reducer)

--- client/components/MyFavourites.jsx
+++ client/components/MyFavourites.jsx
@@ -30,10 +30,10 @@ class MyFavourites extends React.Component {
   }
 
   deleteItem = (recipe_id) => {
-    deleteFavourite(recipe_id)
+    deleteFavourite(recipe_id)//api
       .then(() => {
-        this.props.dispatch(removeFavourite(recipe_id))
-      })
+        this.props.dispatch(removeFavourite(recipe_id))//action
+      }) 
   }
 
   clickHandler = () => {
@@ -53,7 +53,7 @@ class MyFavourites extends React.Component {
           <tbody>
             {this.props.favourites.map((favourite, id) => {
               return (
-                <FavouriteListItem key={id} favourite={favourite} removeFavourite={this.deleteItem} recipe={this.props.recipes} />
+                <FavouriteListItem key={id} favourite={favourite} kill={this.deleteItem} recipe={this.props.recipes}/>
               )
             })}
           </tbody>
