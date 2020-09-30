26d142c90fc77cc93a87ea9a2bfd65853f30a367
Cheryl Wichman <camandche@gmail.com>
2020-09-30 16:49:43 +1300

    Delete Recipe from Favourite

--- server/routes/favourites.test.js
+++ server/routes/favourites.test.js
@@ -1,14 +1,15 @@
 import request from "supertest"
 import server from "../server"
 
-import { getFavourites, addFavourite } from "../db"
+import { getFavourites, addFavourite, deleteFavourite } from "../db"
 
 jest.mock("../db", () => ({
   getFavourites: jest.fn(),
-  addFavourite: jest.fn()
+  addFavourite: jest.fn(),
+  deleteFavourite: jest.fn()
 }))
 
-describe("/api/favourites/:id", () => {
+describe("GET /api/favourites/:id", () => {
   test("returns favourite recipe", () => {
     expect.assertions(2)
     getFavourites.mockImplementation(() =>
@@ -23,7 +24,7 @@ describe("/api/favourites/:id", () => {
   })
 })
 
-describe("/api/favourites", () => {
+describe("ADD /api/favourites", () => {
     test("add fav to database", () => {
        expect.assertions(5)
         addFavourite.mockImplementation(() => Promise.resolve([0]))
@@ -37,5 +38,18 @@ describe("/api/favourites", () => {
                 expect(res.status).toBe(201)
                 expect(res.body.id).toBe(0)
             })
+    })
+})
+
+describe('DELETE /api/favourites/:recipe_id', () => {
+    test("calls deleteFavourite database function", () => {
+        deleteFavourite.mockImplementation(() => Promise.resolve(1))
+        expect.assertions(2)
+        return request(server)
+            .delete('/api/favourites/1')
+            .then(res => {
+                expect(deleteFavourite).toHaveBeenCalledWith(1)
+                expect(res.status).toBe(200)
+        })
     })
 })
