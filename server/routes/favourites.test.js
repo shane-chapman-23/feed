9c5b8131316e63abf007e0156b23ad884d425c60
Cheryl Wichman <camandche@gmail.com>
2020-09-30 16:36:59 +1300

    Add Recipe to Favourites

--- server/routes/favourites.test.js
+++ server/routes/favourites.test.js
@@ -1,10 +1,11 @@
-import request from "supertest";
-import server from "../server";
+import request from "supertest"
+import server from "../server"
 
-import { getFavourites } from "../db";
+import { getFavourites, addFavourite } from "../db"
 
 jest.mock("../db", () => ({
   getFavourites: jest.fn(),
+  addFavourite: jest.fn()
 }))
 
 describe("/api/favourites/:id", () => {
@@ -20,4 +21,21 @@ describe("/api/favourites/:id", () => {
         expect(res.body.length).toBe(1)
       })
   })
+})
+
+describe("/api/favourites", () => {
+    test("add fav to database", () => {
+       expect.assertions(5)
+        addFavourite.mockImplementation(() => Promise.resolve([0]))
+        return request(server)
+            .post('/api/favourites')
+            .send({user_id: 2, recipe_id: 3})
+            .then(res => {
+                expect(addFavourite).toHaveBeenCalled()
+                expect(addFavourite.mock.calls[0][0].user_id).toBe(2)
+                expect(addFavourite.mock.calls[0][0].recipe_id).toBe(3)
+                expect(res.status).toBe(201)
+                expect(res.body.id).toBe(0)
+            })
+    })
 })
