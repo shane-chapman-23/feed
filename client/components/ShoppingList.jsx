dbba19e79481bd38dc17580b45bb71a46207e8a3
chaz-ratima <chaz@ChazPC.localdomain>
2020-09-30 16:34:25 +1300

    fixed issues

--- client/components/ShoppingList.jsx
+++ client/components/ShoppingList.jsx
@@ -9,8 +9,8 @@ class ShoppingList extends React.Component {
     const sorted = sortIngredients(getAllIngredients(this.props.ingredients, favouriteIds))
     const finalList = removeDuplicates(sorted)
     return(
-      <div id="pdf" className="printSpacing">
-        <h2>Shopping List:</h2>
+      <div className="printSpacing">
+        <h2 className="printHidden">Shopping List:</h2>
         <p>
           {finalList.map(list => <li style={{listStyleType: 'none'}} key={list.ingredient_name}>{list.ingredient_name} {list.ingredient_quantity}{list.measurement_name}</li>)}
         </p>
